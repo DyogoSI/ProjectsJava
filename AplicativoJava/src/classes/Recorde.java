f9d911d93690faa02be3316828647065632b4aa0
Dyogo Henrique <dyogohenriquedasilva@gmail.com>
2024-03-23 10:13:32 -0300

    JavaApp

---
+++ AplicativoJava/src/classes/Recorde.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author dyogo
+ */
+public class Recorde {
+    private String nome;
+    private String data;
+    private String tempo;
+
+    public Recorde(String nome, String data, String tempo) {
+        this.nome = nome;
+        this.data = data;
+        this.tempo = tempo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(String tempo) {
+        this.tempo = tempo;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Data: " + data + ", Tempo: " + tempo;
+    }
+}
